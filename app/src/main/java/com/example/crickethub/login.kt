76ac01fb619e52ba9dc6e9957af7b0a5515aaa9d
Student <snehagohil49@gmail.com>
2024-08-24 16:52:12 +0530

    jgg

--- app/src/main/java/com/example/crickethub/login.kt
+++ app/src/main/java/com/example/crickethub/login.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 
 class login : AppCompatActivity() {
@@ -12,10 +13,16 @@ class login : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
         val tv1:TextView=findViewById(R.id.tv1);
+        val b1:Button=findViewById(R.id.b1);
         tv1.setOnClickListener {
             val intent = Intent(this, registration::class.java)
             startActivity(intent)
         }
+        b1.setOnClickListener {
+            val intent = Intent(this, homepage::class.java)
+            startActivity(intent)
+        }
+
 
     }
 }
--- app/src/main/java/com/example/crickethub/registration.kt
+++ app/src/main/java/com/example/crickethub/registration.kt
@@ -1,11 +1,18 @@
 package com.example.crickethub
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class registration : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration2)
+        val reg:Button=findViewById(R.id.reg);
+        reg.setOnClickListener {
+            val intent = Intent(this, login::class.java)
+            startActivity(intent)
+        }
     }
 }
