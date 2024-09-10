56cc3f9a40b9a9eca86d8d9460cb844c5dac85f1
sneha-gohil <snehagohil49@gmail.com>
2024-09-10 10:42:54 +0530

    ven1

--- app/src/main/java/com/example/crickethub/homepage.kt
+++ app/src/main/java/com/example/crickethub/homepage.kt
@@ -1,11 +1,22 @@
 package com.example.crickethub
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class homepage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_homepage)
+
+        val tvv1: TextView = findViewById(R.id.tvv1);
+        val tvv2: TextView = findViewById(R.id.tvv2);
+        val tvv3: TextView = findViewById(R.id.tvv3);
+
+        tvv1.setOnClickListener{
+            val intent = Intent(this, slot::class.java)
+            startActivity(intent)
+        }
     }
 }
