f3a77b9d495cae5122a88c0b8f23e9bf48f004a6
Student <ankitsingh14175@gmail.com>
2024-09-11 13:58:31 +0530

    frag

--- app/src/main/java/com/example/crickethub/homepage.kt
+++ app/src/main/java/com/example/crickethub/homepage.kt
@@ -1,33 +1,29 @@
 package com.example.crickethub
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import viewpg
 
 class homepage : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_homepage)
-
-        val tvv1: TextView = findViewById(R.id.tvv1);
-        val tvv2: TextView = findViewById(R.id.tvv2);
-        val tvv3: TextView = findViewById(R.id.tvv3);
-
-        tvv1.setOnClickListener {
-            val intent = Intent(this, slot::class.java)
+        val vp:ViewPager2=findViewById(R.id.vp);
+        val i1 =findViewById<ImageView>(R.id.i1);
+       // val i2:ImageView=findViewById(R.id.i2);
+        val adapter = viewpg(this)
+        vp.adapter = adapter
+        i1.setOnClickListener{
+            val intent = Intent(this, slot_booking::class.java)
             startActivity(intent)
-        }
-        class HomeFragment : Fragment(R.layout.fragment_home) {
-        }
-
-        class ProfileFragment : Fragment(R.layout.fragment_profile) {
-        }
-
-        class HistoryFragment : Fragment(R.layout.fragment_history) {
-
         }
     }
 }
